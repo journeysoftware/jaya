e585c9e90de4e4a4026b0f279b4346bbf7d920e9
Benze Gong <benzebgong@gmail.com>
2018-03-15 17:07:07 -0700

    payload addition - nudge

---
+++ nudge/message.go
@@ -0,0 +1,32 @@
+// Package nudge contains utility functions for building Slack messages from nudge notifications
+package nudge
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type IssuesDevstreamActivityPayload struct {
+	Issue struct {
+		Key string `json:"key"`
+	}
+	Activity string `json:"activity"`
+	Feedback struct {
+		Text     string     `json:"text"`
+		Timeline [][]string `json:"timeline"`
+	}
+}
+
+//IssuesDevstreamActivity ...
+func IssuesDevstreamActivity(body []byte) string {
+	var m = IssuesDevstreamActivityPayload{}
+	err := json.Unmarshal(body, &m)
+	if err != nil {
+		fmt.Println("error:", err)
+		return "error"
+	}
+	issueID := m.Issue.Key
+	feedback := m.Feedback
+	message := issueID + " possibly has " + feedback.Text
+	return message
+}
